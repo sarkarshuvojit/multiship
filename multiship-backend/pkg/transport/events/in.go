6559aee4176a407116028c5259a688daa2cb34cc
Shuvojit <s15sarkar@yahoo.com>
2025-06-07 00:50:31 +0530

    added more event types

--- multiship-backend/pkg/transport/events/in.go
+++ multiship-backend/pkg/transport/events/in.go
@@ -3,6 +3,10 @@ package events
 type InboundEventType string
 
 const (
-	Signup     InboundEventType = "Signup"
-	CreateRoom                  = "CreateRoom"
+	Signup      InboundEventType = "SIGN_UP"
+	CreateRoom                   = "CREATE_ROOM"
+	JoinRoom                     = "JOIN_ROOM"
+	SubmitBoard                  = "SUBMIT_BOARD"
+	TryHit                       = "TRY_HIT"
+	StartGame                    = "START_GAME"
 )
--- multiship-backend/pkg/transport/events/out.go
+++ multiship-backend/pkg/transport/events/out.go
@@ -3,8 +3,13 @@ package events
 type OutboundEventType string
 
 const (
-	SignedUp     OutboundEventType = "SignedUp"
-	RoomCreated                    = "RoomCreated"
-	GeneralError                   = "GeneralError"
-	Welcome                        = "Welcome"
+	SignedUp      OutboundEventType = "SIGNED_UP"
+	RoomCreated                     = "ROOM_CREATED"
+	GeneralError                    = "GENERAL_ERROR"
+	Welcome                         = "WELCOME"
+	RoomJoined                      = "ROOM_JOINED"
+	NextTurn                        = "NEXT_TURN"
+	HitSuccessful                   = "HIT_ATTEMPTED"
+	HitFailed                       = "HIT_FAILED"
+	BoardUpdated                    = "BOARD_UPDATED"
 )
