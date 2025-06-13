2a63ddace6d29b219bebbaba1dc0a2fe01f3b67c
Shuvojit <s15sarkar@yahoo.com>
2025-06-14 00:01:52 +0530

    created util to easily create clients

--- multiship-backend/tests/integration/three_player_room_test.go
+++ multiship-backend/tests/integration/three_player_room_test.go
@@ -1,101 +1,13 @@
 package integration
 
 import (
-	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/sarkarshuvojit/multiship-backend/internal/api/events"
-	"github.com/sarkarshuvojit/multiship-backend/internal/api/utils"
 	"github.com/stretchr/testify/assert"
 )
-
-type TestClient struct {
-	conn     *websocket.Conn
-	messages chan events.OutboundEvent
-	errors   chan error
-	done     chan struct{}
-	email    string
-}
-
-func NewTestClient(serverURL string) (*TestClient, error) {
-	dialer := &websocket.Dialer{}
-	conn, _, err := dialer.Dial(serverURL, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	client := &TestClient{
-		conn:     conn,
-		messages: make(chan events.OutboundEvent, 10),
-		errors:   make(chan error, 10),
-		done:     make(chan struct{}),
-	}
-
-	go client.readMessages()
-	return client, nil
-}
-
-func (c *TestClient) readMessages() {
-	defer close(c.done)
-
-	for {
-		_, msgBytes, err := c.conn.ReadMessage()
-		if err != nil {
-			c.errors <- err
-			return
-		}
-
-		var msg events.OutboundEvent
-		if err := json.Unmarshal(msgBytes, &msg); err != nil {
-			c.errors <- err
-			continue
-		}
-
-		c.messages <- msg
-	}
-}
-
-func (c *TestClient) SendMessage(msgType events.InboundEventType, data any) error {
-	dataRaw := utils.QuickMarshal(data)
-	msg := events.InboundEvent{
-		EventType: msgType,
-		Payload:   []byte(dataRaw),
-	}
-
-	msgBytes, err := json.Marshal(msg)
-	if err != nil {
-		return err
-	}
-
-	return c.conn.WriteMessage(websocket.TextMessage, msgBytes)
-}
-
-func (c *TestClient) WaitForMessage(expectedType events.OutboundEventType, timeout time.Duration) (*events.OutboundEvent, error) {
-	timer := time.NewTimer(timeout)
-	defer timer.Stop()
-
-	for {
-		select {
-		case msg := <-c.messages:
-			if msg.EventType == expectedType {
-				return &msg, nil
-			}
-		case err := <-c.errors:
-			return nil, err
-		case <-timer.C:
-			return nil, fmt.Errorf("timeout waiting for message type: %s", expectedType)
-		case <-c.done:
-			return nil, fmt.Errorf("connection closed")
-		}
-	}
-}
-
-func (c *TestClient) Close() error {
-	return c.conn.Close()
-}
 
 func TestMultiClientRoomInteraction(t *testing.T) {
 	url := "ws://localhost:5000/ws"
---
+++ multiship-backend/tests/integration/utils.go
@@ -0,0 +1,126 @@
+package integration
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/sarkarshuvojit/multiship-backend/internal/api"
+	"github.com/sarkarshuvojit/multiship-backend/internal/api/events"
+	"github.com/sarkarshuvojit/multiship-backend/internal/api/handlers"
+	"github.com/sarkarshuvojit/multiship-backend/internal/api/state"
+	"github.com/sarkarshuvojit/multiship-backend/internal/api/utils"
+)
+
+type TestClient struct {
+	conn     *websocket.Conn
+	messages chan events.OutboundEvent
+	errors   chan error
+	done     chan struct{}
+	email    string
+}
+
+func NewTestClient(serverURL string) (*TestClient, error) {
+	dialer := &websocket.Dialer{}
+	conn, _, err := dialer.Dial(serverURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &TestClient{
+		conn:     conn,
+		messages: make(chan events.OutboundEvent, 10),
+		errors:   make(chan error, 10),
+		done:     make(chan struct{}),
+	}
+
+	go client.readMessages()
+	return client, nil
+}
+
+func (c *TestClient) readMessages() {
+	defer close(c.done)
+
+	for {
+		_, msgBytes, err := c.conn.ReadMessage()
+		if err != nil {
+			c.errors <- err
+			return
+		}
+
+		var msg events.OutboundEvent
+		if err := json.Unmarshal(msgBytes, &msg); err != nil {
+			c.errors <- err
+			continue
+		}
+
+		c.messages <- msg
+	}
+}
+
+func (c *TestClient) SendMessage(msgType events.InboundEventType, data any) error {
+	dataRaw := utils.QuickMarshal(data)
+	msg := events.InboundEvent{
+		EventType: msgType,
+		Payload:   []byte(dataRaw),
+	}
+
+	msgBytes, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	return c.conn.WriteMessage(websocket.TextMessage, msgBytes)
+}
+
+func (c *TestClient) WaitForMessage(expectedType events.OutboundEventType, timeout time.Duration) (*events.OutboundEvent, error) {
+	timer := time.NewTimer(timeout)
+	defer timer.Stop()
+
+	for {
+		select {
+		case msg := <-c.messages:
+			if msg.EventType == expectedType {
+				return &msg, nil
+			}
+		case err := <-c.errors:
+			return nil, err
+		case <-timer.C:
+			return nil, fmt.Errorf("timeout waiting for message type: %s", expectedType)
+		case <-c.done:
+			return nil, fmt.Errorf("connection closed")
+		}
+	}
+}
+
+func (c *TestClient) Close() error {
+	return c.conn.Close()
+}
+
+func StartWebsocketServer() func() {
+	ctx, cancel := context.WithCancel(
+		context.Background(),
+	)
+	go func(context context.Context) {
+		wt := api.NewWebsocketTransport()
+		wt.InitHandlers()
+
+		// Add Dependencies
+		db, err := state.NewRedisState("localhost:6379", 0, "localpass")
+		if err != nil {
+			panic("Cannot connect to redis")
+		}
+		wt.AddDependency(utils.Redis, db)
+
+		// Add event handlers
+		wt.HandleEvent(events.Signup, handlers.SignupHandler)
+		wt.HandleEvent(events.CreateRoom, handlers.CreateRoomHandler)
+		wt.HandleEvent(events.JoinRoom, handlers.JoinRoomHandler)
+		http.ListenAndServe(":5000", nil)
+	}(ctx)
+
+	return cancel
+}
