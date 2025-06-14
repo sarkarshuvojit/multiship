ea32507a089c560c24a67e5b127a44becdf85624
Shuvojit <s15sarkar@yahoo.com>
2025-06-14 15:26:21 +0530

    added base url

--- multiship-ui/vite.config.ts
+++ multiship-ui/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/multiship/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
