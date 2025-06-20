3fbbbfeb8ad9f7c2332377054f86c4c704f07627
Shuvojit <s15sarkar@yahoo.com>
2025-06-20 23:33:18 +0530

    basic piece validation added

--- multiship-backend/internal/game/utils.go
+++ multiship-backend/internal/game/utils.go
@@ -22,12 +22,47 @@ func getWord() string {
 	return b.String()
 }
 
+func validatePieceFrequency(ships []ShipState) bool {
+	if len(ships) != 10 {
+		return false
+	}
+	lenfreq := map[int]int{}
+	for _, ship := range ships {
+		if cur, ok := lenfreq[ship.Len]; ok {
+			lenfreq[ship.Len] = cur + 1
+		} else {
+			lenfreq[ship.Len] = 1
+		}
+	}
+	expectedLenfreq := map[int]int{
+		1: 4, //(Battleship)
+		2: 3, //(Cruisers)
+		3: 2, //(Destroyers)
+		4: 1, //(Submarines)
+	}
+	for key, value := range lenfreq {
+		if expectedLenfreq[key] != value {
+			return false
+		}
+	}
+	return true
+}
+
 // validateBoard validates whether a ship configuration can be placed on the board without any conflicts
 // Conflicts may include
 // - Incorrect amount of ships of specific lengths
 // - Position overlaps
 // - Position exceeding bounds
 // - Position right next to another ship
-func validateBoard(_ []ShipState) bool {
-	return false
+//
+// Required Ships
+// 1 ship of length  4 (Battleship)
+// 2 ships of length 3 (Cruisers)
+// 3 ships of length 2 (Destroyers)
+// 4 ships of length 1 (Submarines)
+func validateBoard(ships []ShipState) bool {
+	if !validatePieceFrequency(ships) {
+		return false
+	}
+	return true
 }
